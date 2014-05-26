862731c7860a570b95a139e15839cfb2c683ad22
Prajas <mymac@Mys-Mac-Pro-2.local>
2014-05-26 12:45:24 +0530

    Initial Commit

---
+++ mydoorview/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mydoorview
+//
+//  Created by Prajas on 5/26/14.
+//  Copyright (c) 2014 My Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mydoorview/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mydoorview
+//
+//  Created by Prajas on 5/26/14.
+//  Copyright (c) 2014 My Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
